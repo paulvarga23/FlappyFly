2e994757075e498bde42e0b309d18fa7d08df06a
paulvarga23 <paul_varga@me.com>
2014-09-10 12:34:46 +0100

    Initial Commit

---
+++ Goal.h
@@ -0,0 +1,13 @@
+//
+//  Goal.h
+//  FlappyFly
+//
+//  Created by Paul Varga on 08.09.14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Goal : CCNode
+
+@end
---
+++ Obstacle.h
@@ -0,0 +1,15 @@
+//
+//  Obstacle.h
+//  FlappyFly
+//
+//  Created by Paul Varga on 06.09.14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Obstacle : CCNode
+
+- (void)setupRandomPosition;
+
+@end
